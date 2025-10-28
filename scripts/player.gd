9d36d5fbad5d98920d5b26ad1ca7ed86c25b741e
Omay238 <238maculo238@gmail.com>
2025-10-27 18:26:52 -0600

    player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,6 @@
-extends AnimatableBody2D
+extends RigidBody2D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down");
 	
+	apply_central_force(input * 40000);
