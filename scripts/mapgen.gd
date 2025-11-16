2c4658b79b5eb7dd905cc2dac20f420f5c5e6980
Omay238 <238maculo238@gmail.com>
2025-11-15 23:32:02 -0700

    player positioning and 1/2 elevator

--- scripts/mapgen.gd
+++ scripts/mapgen.gd
@@ -24,26 +24,36 @@ func _ready():
 	
 	return generator:CellToTiles(dungeon)""" % [32 + Vars.id, 32 + Vars.id])
 	
-	var map_str = "";
+	var map_str = ""
 	
-	var path_cells = [];
-	var wall_cells = [];
+	var path_cells = []
+	var wall_cells = []
+	var elevator_pos = Vector2.ZERO
 	
 	for y in range(0, map.get(0).length() + 1):
 		for x in range(0, map.length() + 1):
-			if map.get(x).get(y) == "#":
+			var ch = map.get(x).get(y)
+			if ch == "#":
 				wall_cells.append(Vector2i(x * 2, y * 2))
 				wall_cells.append(Vector2i(x * 2 + 1, y * 2))
 				wall_cells.append(Vector2i(x * 2, y * 2 + 1))
 				wall_cells.append(Vector2i(x * 2 + 1, y * 2 + 1))
-			else:
+			else: # door or floor
+				if ch == "A":
+					$"../Player".position = Vector2(x * 256, y * 256)
+				elif ch == "B":
+					elevator_pos = Vector2(x * 2, y * 2)
 				path_cells.append(Vector2i(x * 2, y * 2))
 				path_cells.append(Vector2i(x * 2 + 1, y * 2))
 				path_cells.append(Vector2i(x * 2, y * 2 + 1))
 				path_cells.append(Vector2i(x * 2 + 1, y * 2 + 1))
-			map_str += map.get(x).get(y)
+			map_str += ch
 		map_str += "\n"
 	print(map_str)
 	
 	set_cells_terrain_connect(wall_cells, Vars.spooky_level, 0)
 	set_cells_terrain_connect(path_cells, Vars.spooky_level, 1)
+	
+	for x in range(elevator_pos.x, elevator_pos.x + 2):
+		for y in range(elevator_pos.y, elevator_pos.y + 2):
+			set_cell(Vector2i(x, y), 0, Vector2i(12, 4))
